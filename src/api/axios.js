bcd3b53cfc4913c45aaaa8a785f27e71ef8b47cb
Dao Luong <daovanluongpdl@gmail.com>
2021-11-09 14:17:06 +0700

    fix axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,6 +5,7 @@ export const cryptoInstance = axios.create({
   headers: {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
     'x-rapidapi-key': 'b566622117msh553f4288430138ap1d3a79jsnce3480e7da91',
+    'x-access-token': 'i-have-to-migrate-to-v2'
   },
 });
 
@@ -12,6 +13,7 @@ export const cryptoNewsInstance = axios.create({
   baseURL: 'https://bing-news-search1.p.rapidapi.com/news',
   headers: {
     'x-bingapis-sdk': 'true',
+    'x-access-token': 'i-have-to-migrate-to-v2',
     'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
     'x-rapidapi-key': 'b566622117msh553f4288430138ap1d3a79jsnce3480e7da91',
   },
